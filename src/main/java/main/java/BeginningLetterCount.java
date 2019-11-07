8b5cb5edd8a22a4dc787013d1bd618162d22cd05
Jakob Schwerter <jakob.schwerter@googlemail.com>
2019-11-07 14:04:28 +0100

    testat 1, aufgabe 1

--- src/main/java/main/java/BeginningLetterCount.java
+++ src/main/java/main/java/BeginningLetterCount.java
@@ -7,7 +7,6 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
-import org.apache.spark.api.java.function.Function;
 import scala.Tuple2;
 
 public class BeginningLetterCount {
@@ -17,12 +16,7 @@ public class BeginningLetterCount {
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> lines = sc.textFile(args[0]);
         JavaRDD<String> words = lines.flatMap(l -> Arrays.asList(l.split("[^\\w]+")).iterator());
-        JavaRDD<String> wordsFiltered = words.filter(new Function<String, Boolean>() {
-            @Override
-            public Boolean call(String v1) throws Exception {
-                return (v1.length() > 0 && Character.isLetter(v1.charAt(0)));
-            }
-        });
+        JavaRDD<String> wordsFiltered = words.filter(w -> (w.length() > 0 && Character.isLetter(w.charAt(0))));
         JavaPairRDD<Character, Integer> wordsLetters = wordsFiltered.mapToPair(w -> new Tuple2<Character, Integer>(w.toUpperCase().charAt(0), 1));
         JavaPairRDD<Character, Integer> counts = wordsLetters.reduceByKey((n1, n2) -> n1 + n2);
         counts.saveAsTextFile(args[1]);
