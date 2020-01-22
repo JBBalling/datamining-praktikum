46873e8c7e3ee3d3b33adf2e9c191b3fe056eb42
Jakob Schwerter <jakob.schwerter@googlemail.com>
2020-01-22 14:59:37 +0100

    LSH

---
+++ src/main/java/testat02/MinHashingAndLSH/HashFunction.java
@@ -0,0 +1,28 @@
+package testat02.MinHashingAndLSH;
+
+import java.util.function.Function;
+
+public class HashFunction implements java.io.Serializable {
+
+    private int a;
+    private int b;
+    private int primeNumber = 131071;
+    private int minHashSignatureSize = 1000;
+
+    private int randomLowerBound = 1;
+    private int randomUpperBound = 1000;
+
+    HashFunction() {
+        a = randomNumber();
+        b = randomNumber();
+    }
+
+    private int randomNumber() {
+        return (int) ((Math.random() * (randomUpperBound - randomLowerBound)) + randomLowerBound);
+    }
+
+    int hash(int x) {
+        return ((((a * x) + b) % primeNumber) % minHashSignatureSize);
+    }
+
+}
--- src/main/java/testat02/MinHashingAndLSH/LSH.java
+++ src/main/java/testat02/MinHashingAndLSH/LSH.java
@@ -8,11 +8,14 @@ import org.apache.spark.broadcast.Broadcast;
 import org.apache.spark.ml.linalg.Vectors;
 import org.apache.spark.sql.sources.In;
 import scala.Tuple2;
+import com.google.common.hash.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Vector;
+import java.util.function.Function;
 
 /**
  * sparkSubmit --class testat02.MinHashingAndLSH.LSH target/data-mining-praktikum-1.0-SNAPSHOT.jar
@@ -26,10 +29,10 @@ public class LSH implements java.io.Serializable {
 
     private int k = 3;
 
+    private int numberHashFunctions = 3;
+
     private static int minHashSignatureSize = 1000;
     private static int primeNumber = 131071;
-    private static int randomNumberLowerBound = 0;
-    private static int randomNumberUpperBound = 1000;
 
     public static void main(String[] args) {
         LSH lsh = new LSH();
@@ -37,7 +40,7 @@ public class LSH implements java.io.Serializable {
     }
 
     void main() {
-        conf = new SparkConf();
+        conf = new SparkConf().set("spark.executor.memory","8G");
         jsc = new JavaSparkContext(conf);
         Broadcast<Integer> shingleSize = jsc.broadcast(k);
         JavaRDD<Review> lines = jsc.parallelize(jsc.textFile(path).take(1000)) // jsc.textFile(path) für alle Zeilen
@@ -59,30 +62,59 @@ public class LSH implements java.io.Serializable {
         tempArray = shingleList.toArray(tempArray);
         Broadcast<String[]> oneHotReference = jsc.broadcast(tempArray);
 
-        JavaPairRDD<Integer, int[]> oneHot = lines.mapToPair(l -> {
-            int[] oneHotArr = new int[oneHotReference.value().length];
+        JavaPairRDD<Integer, List<Integer>> oneHot = lines.mapToPair(l -> {
+            List<Integer> oneHotArr = new ArrayList<>(oneHotReference.value().length);
             for (int i = 0; i < oneHotReference.value().length; i++) {
                 if (l.getShingles().contains(oneHotReference.value()[i])) {
-                    oneHotArr[i] = 1;
+                    oneHotArr.add(i, 1);
                 } else {
-                    oneHotArr[i] = 0;
+                    oneHotArr.add(i, 0);
                 }
             }
-            return new Tuple2<Integer, int[]>(l.getID(), oneHotArr);
+            return new Tuple2<Integer, List<Integer>>(l.getID(), oneHotArr);
         });
 
+        HashFunction[] hashFunctions = new HashFunction[numberHashFunctions];
+        for (int i = 0; i < hashFunctions.length; i++) {
+            hashFunctions[i] = new HashFunction();
+        }
+        Broadcast<HashFunction[]> hashFunctionsBroadcast = jsc.broadcast(hashFunctions);
+
+        JavaPairRDD<Integer, List<Integer>> permutations = oneHot.mapToPair(m -> {
+            List<Integer> list = new ArrayList<>();
+            for (HashFunction h : hashFunctionsBroadcast.value()) {
+                list.add(h.hash(m._1));
+            }
+            return new Tuple2<Integer, List<Integer>>(m._1, list);
+        });
+
+        Broadcast<List<Tuple2<Integer, List<Integer>>>> permutationsBroadcast = jsc.broadcast(permutations.sortByKey(true).collect());
+
+        JavaPairRDD<Integer, List<Integer>> signatures = oneHot.flatMapToPair(m -> { // (C_x, (h_1_1, h_2_1, ..., h_x_1))
+                    List<Tuple2<Tuple2<Integer, Integer>, List<Integer>>> list = new ArrayList<>(); // ((C_x, 0/1), (h1, h2, h3, ...))
+                    for (int i = 0; i < m._2.size(); i++) {
+                        list.add(
+                                new Tuple2<Tuple2<Integer, Integer>, List<Integer>>(
+                                        new Tuple2<Integer, Integer>(m._1, m._2.get(i)),
+                                        permutationsBroadcast.value().get(m._1 - 1)._2
+                                )
+                        );
+                    }
+                    return list.iterator();
+                })
+                .filter(f -> f._1._2 == 1)
+                .mapToPair(m -> new Tuple2<Integer, List<Integer>>(m._1._1, m._2))
+                .reduceByKey((n1, n2) -> {
+                    List<Integer> list = new ArrayList<>();
+                    for (int i = 0; i < n1.size(); i++) {
+                        list.add(Math.min(n1.get(i), n2.get(i)));
+                    }
+                    return list;
+                });
 
 
+        signatures.foreach(s -> System.out.println(s));
 
-    }
-
-    static int h(int x) {
-        // (( a * x + b) mod p) mod N
-        return ((((randomNumber() * x) + randomNumber()) % primeNumber) % minHashSignatureSize);
-    }
-
-    static int randomNumber() {
-        return (int) ((Math.random() * (randomNumberUpperBound - randomNumberLowerBound)) + randomNumberLowerBound);
     }
 
     static String arrayAsString(int[] arr) {
@@ -95,5 +127,7 @@ public class LSH implements java.io.Serializable {
         stringBuilder.append("]");
         return stringBuilder.toString();
     }
+
+
 
 }
